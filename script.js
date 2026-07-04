78e732cbdc52079060ebceeee2811fa8e4de9735
Siya Priya <siya87774@gmail.com>
2026-07-04 23:20:39 +0530

    Added Login Dashboard

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log("Smart Invest Hub Loaded Successfully");
+function loginUser(){
+
+let username =
+document.getElementById("username").value;
+
+let password =
+document.getElementById("password").value;
+
+if(username==="admin" && password==="1234"){
+
+localStorage.setItem("loggedIn","true");
+
+window.location.href="dashboard.html";
+
+}
+else{
+
+document.getElementById("message").innerHTML =
+"Invalid Username or Password";
+
+}
+
+}
+
+function logoutUser(){
+
+localStorage.removeItem("loggedIn");
+
+window.location.href="login.html";
+
+}
+
+function calculateProfit(){
+
+let amount =
+document.getElementById("amount").value;
+
+if(amount===""){
+return;
+}
+
+let result =
+amount * 1.15;
+
+document.getElementById("result").innerHTML =
+"Estimated Return: ₹" + result;
+
+}
+
+if(
+window.location.pathname.includes("dashboard.html")
+){
+
+if(
+localStorage.getItem("loggedIn")!=="true"
+){
+
+window.location.href="login.html";
+
+}
+
+}
